181cf5b9c0ececb77643eac304fea3581c6c3bac
Mark Burrell <markrb0609@gmail.com>
2018-06-05 14:06:31 -0700

    Renamed some feild to be more sensible

--- src/app/src/main/java/com/example/mark/passmanurl/UI.java
+++ src/app/src/main/java/com/example/mark/passmanurl/UI.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class UI extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mark.passmanurl.MESSAGE";
     @Override
@@ -17,9 +15,12 @@ public class UI extends AppCompatActivity {
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, PassDisplay.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        EditText editText = (EditText) findViewById(R.id.usrURL);
+        String userURL = editText.getText().toString();
+        editText = (EditText) findViewById(R.id.usrUserName);
+        String userName = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, userName);
         startActivity(intent);
     }
 }
